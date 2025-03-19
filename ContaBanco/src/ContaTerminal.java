53f15846cc2517fe4d446d0bd69f25ac48bbe0d3
Alice <aliceoqueiroz52@gmail.com>
2025-03-19 15:08:41 -0300

    mudando nome classe

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ContaBanco {
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
        Scanner sc = new Scanner(System.in);
 
